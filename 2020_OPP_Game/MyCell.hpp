481ad2478a3d13a44fd204e591bbc9b0b9cfc0bc
LyubimovVladimir <55154058+VladimirLyubimov@users.noreply.github.com>
2020-09-27 20:58:53 +0300

    20200927_OOP_Game_v1.2.1
    Add MyCell.hpp

---
+++ 2020_OPP_Game/MyCell.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+enum State {STATE_GROUND, STATE_WALL, STATE_START, STATE_FINISH};
+
+class MyCell{//класс клетки
+    private:
+        int m_passable;//проходимость клетки
+        State m_type;//её вид (стена, обычная клетка и тд.)
+        int m_touched;//посещенали клетка при генерации лабиринта
+        int m_x;
+        int m_y;
+    public:
+    	MyCell();
+    	MyCell(const MyCell &cell);
+    	void setData(int set_passable, State set_type);
+    	void setCoordinates(int set_x, int set_y);
+    	void getCoordinates(int &get_x, int &get_y);
+    	void print();
+    	void setAttendance();
+    	int getAttendance();
+    	~MyCell();
+};
