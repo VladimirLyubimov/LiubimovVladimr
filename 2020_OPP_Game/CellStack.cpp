58ad90a84999e68738acc3b5d5e7cceefd9d4b8f
LyubimovVladimir <55154058+VladimirLyubimov@users.noreply.github.com>
2020-10-04 19:59:51 +0300

    Update CellStack.cpp

--- 2020_OPP_Game/CellStack.cpp
+++ 2020_OPP_Game/CellStack.cpp
@@ -1,27 +1,27 @@
 #include "CellStack.hpp"
 
-	CellStack::CellStack(int size){
-        m_data = new MyCell[size];
-        m_length = 0;
-    }
-        
-    void CellStack::Push(MyCell cell){//добавления клетки в стек            
-		m_data[m_length] = cell;
-        m_length++;
-    }
-    
-    MyCell CellStack::Top(){//получение верхего элемента стека
-    	return m_data[m_length-1];
-    }
-    
-    int CellStack::getLength(){//получение количества элементов в стеке
-        return this->m_length;
-    }
-           
-    void CellStack::Remove(){//удаление верхнего элемента
-        m_length--;
-    }
-    
-    CellStack::~CellStack(){//деструктор; очищает выделенную память
-        delete[] m_data;
-    }
+CellStack::CellStack(int size) {
+    m_data = new MyCell[size];
+    m_length = 0;
+}
+
+void CellStack::Push(MyCell cell) {//добавления клетки в стек
+    m_data[m_length] = cell;
+    m_length++;
+}
+
+MyCell CellStack::Top() {//получение верхего элемента стека
+    return m_data[m_length - 1];
+}
+
+int CellStack::getLength() {//получение количества элементов в стеке
+    return this->m_length;
+}
+
+void CellStack::Remove() {//удаление верхнего элемента
+    m_length--;
+}
+
+CellStack::~CellStack() {//деструктор; очищает выделенную память
+    delete[] m_data;
+}
