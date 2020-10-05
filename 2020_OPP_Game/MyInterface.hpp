55cf8e4f91e06fe351239f9a095cb829caa2ec2d
LyubimovVladimir <55154058+VladimirLyubimov@users.noreply.github.com>
2020-10-05 18:26:56 +0300

    Update MyInterface.hpp

--- 2020_OPP_Game/MyInterface.hpp
+++ 2020_OPP_Game/MyInterface.hpp
@@ -7,5 +7,5 @@ class MyInterface{//класс реализующий примитивный к�
 		void getStartOfGenerationCoordinates(int &x, int &y);//получает координаты точки старта генерации с консоли
 		void getMazeSize(int &x, int &y);//получение линейных размеров лабиринта
 		void getStartAndFinish(int &xs, int &ys, int &xf, int &yf);//получает точки старта и финиша
-		void printMaze(MyMaze maze);
+		void printMaze(MyMaze &maze);
 };		
