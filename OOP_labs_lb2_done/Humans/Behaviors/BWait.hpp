9a3c41756cb963e2b5bfa854e295f9c34842444f
LyubimovVladimir <55154058+VladimirLyubimov@users.noreply.github.com>
2020-12-22 01:30:28 +0300

    Delete BWait.hpp

--- OOP_labs_lb2_done/Humans/Behaviors/BWait.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Behavior.hpp"
-#include <ctime>
-#include <cstdlib>
-
-class BWait: public Behavior{
-	public:
-		virtual void Action(MyHuman* first, MyHuman* second, int dx, int dy);
-};
