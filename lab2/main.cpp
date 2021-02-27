16cf6bf12dc3ebc47e7a35b01065746110cce565
LyubimovVladimir <55154058+VladimirLyubimov@users.noreply.github.com>
2021-02-27 23:26:29 +0300

    Add files via upload

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -65,9 +65,10 @@ class H_list{
 			return m_head;
 		}
 				
-		int makeList(string& data, int i, Node* cur){
+		int makeList(string& data, int i, Node* cur, int level){
 			if(data == ""){
 				m_head = new Node(0);
+				cout << "The empty hierarchical created.\n";
 				return 0;
 			}
 			
@@ -76,6 +77,7 @@ class H_list{
 					m_head = new Node(data[0]);
 					cur = m_head;
 					i += 1;
+					cout << "The first element created. Its value is " << data[0] << "\n";
 					continue;
 				}
 				
@@ -83,6 +85,7 @@ class H_list{
 					i += 1;
 					m_head = new Node(data[i]);
 					cur = m_head;
+					cout << "The first element created. Its value is " << data[i] << "\n";
 					i += 1;
 					continue;
 				}
@@ -91,16 +94,25 @@ class H_list{
 					i += 1;
 					cur->setChild(new Node(data[i]));
 					i += 1;
-					i = makeList(data, i, cur->getChild());
+					for(int j = 0; j < level; j++)
+						cout << "\t";
+					cout << "The building of new level of hierarchical list have been started. Recursion used.\n";
+					i = makeList(data, i, cur->getChild(), level+1);
 					continue;
 				}
 				
 				if(data[i] == ')'){
 					i += 1;
+					for(int j = 0; j < level; j++)
+						cout << "\t";
+					cout << "Return to previos level of the list.\n";
 					return i;
 				}
 				
 				if(data[i] != ')' && data[i] != '('){
+					for(int j = 0; j < level; j++)
+						cout << "\t";
+					cout << "The one more element created. Its value is " << data[i] << "\n";
 					cur->setNext(new Node(data[i]));
 					cur = cur->getNext();
 					i += 1;
@@ -110,16 +122,56 @@ class H_list{
 			
 			return 0;
 		}
+		
+		void replaceAtom(char atom, char change, Node* cur){
+			while(cur){
+				if(cur->getChild()){
+					replaceAtom(atom, change, cur->getChild());
+				}
+				
+				if(cur->getData() == atom){
+					cur->setData(change);
+				}
+				
+				cur = cur->getNext();
+			}
+		}
+		
+		void clearSpace(Node* cur){
+			Node* prev;
+			while(cur){
+				if(cur->getChild()){
+					clearSpace(cur->getChild());
+				}
+				prev = cur;
+				cur = cur->getNext();
+				delete prev;
+			}
+			return;
+		}
+		
+		~H_list(){
+			clearSpace(m_head);
+		}
 };
+
+void makeLog(ofstream& fout, string message){
+	fout << message;
+	cout << message;
+}
 
 int main(){
 	H_list list;
 	string str = "";
-	string data = "(1(2(3(4(5(6(7))))))7)";
-	list.makeList(data, 0, list.getHead());
+	string data = "(1(2(3(4(5(6(7))))3(45)))7)";
+	list.makeList(data, 0, list.getHead(), 0);
 	list.getPrintMessage(str, list.getHead());
+	list.replaceAtom('7', '!', list.getHead());
 	//cout << list.getHead().getData() << "\n";
 	//cout << list.getCur().getData() << "\n";
+	cout << str << "\n";
+	str = "";
+	list.getPrintMessage(str, list.getHead());
 	cout << str << "\n";
 	return 0;
 }
