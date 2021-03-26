28220aae6ac8c8919dd853054ff609f7a0c956be
LyubimovVladimir <55154058+VladimirLyubimov@users.noreply.github.com>
2021-03-26 21:44:02 +0300

    Create main.cpp

---
+++ lab5/main.cpp
@@ -0,0 +1,115 @@
+#include <regex.h>
+#include <string.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm> //for swap()
+
+using namespace std;
+
+class Dheap{
+	private:
+		int* m_arr = nullptr;
+		int m_root = 0;
+		int m_size = 0;
+		int m_d = 2;
+
+	public:
+		Dheap(int* arr = nullptr, int root = 0, int size = 0, int d = 2): m_arr(arr), m_root(root), m_size(size), m_d(d){
+		}
+
+		int findMaxLeaf(int root){
+			int max = root;
+			for(int i = root; i < m_size || i <= root + m_d; i++){
+				if(m_arr[i] > m_arr[max]){
+					max = i;
+				}
+			}
+			return max;
+		}
+
+		void siftUp(int leaf){
+			if(leaf == m_root || (leaf-1)/m_d < 0){
+				return;
+			}
+
+			while(leaf != m_root && m_arr[leaf] > m_arr[(leaf-1)/m_d]){
+				swap(m_arr[leaf], m_arr[(leaf-1)/m_d]);
+				leaf = (leaf-1)/m_d;
+				if(leaf < 0){
+					return;
+				}
+			}
+		}
+
+		void siftDown(int root){
+			while(root * m_d + 1 < m_size){
+				if(m_arr[root] > m_arr[findMaxLeaf(root)]){
+					return;
+				}
+
+				int c = m_arr[root]; 
+				int n_root = findMaxLeaf(root);
+				m_arr[root] = m_arr[n_root];
+				m_arr[n_root] = c;
+				root = n_root;
+			}
+		}
+
+		void makeHeap(){
+			int i = m_size/m_d;
+			while(i >= 0){
+				siftDown(i);
+				i -= 1;
+			}
+		}
+
+		void addElement(){}
+		
+		int dragMax(){
+			int max = m_arr[m_root];
+			m_arr[m_root] = m_arr[m_size-1];
+			m_size -= 1;
+			return max;
+		}
+
+		void printHeap(){
+			int lev = 1;
+			for(int i = 0; i < m_size; i++){
+				cout << m_arr[i] << ' ';
+				//if(i%(m_d*lev) == 0){
+					//lev += 1;
+				//	cout << '\n';
+				//}
+			}
+			cout << '\n';
+		}
+
+		~Dheap(){
+			delete[] m_arr;
+		}
+};
+
+int main(){
+	int* arr = new int[10];
+	ifstream fin("test.txt");
+	if(!fin.is_open()){
+		cout << "Opening file with test data failed!\n";
+		return 0;
+	}
+	for(int i = 0; i < 10; i++){
+		fin >> arr[i];
+	}
+	
+	for(int i = 0; i < 10; i++){
+		cout << arr[i] << ' ';
+	}
+	cout << '\n';
+
+	Dheap heap(arr, 0, 10, 2);
+	heap.makeHeap();
+	heap.printHeap();
+	return 0;
+}
