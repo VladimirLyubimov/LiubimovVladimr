dc690c90e377d09559632f7766e0bcf375bcff4c
LyubimovVladimir <55154058+VladimirLyubimov@users.noreply.github.com>
2021-05-15 12:35:03 +0300

    Update lab2.cpp

--- 2021_PIAA/lab2.cpp
+++ 2021_PIAA/lab2.cpp
@@ -9,16 +9,13 @@ using std::cout;
 using std::cin;
 
 void getGraph(int**& graph) {
-    std::string st;
-    while (1) {
-        getline(cin, st);
-        if (st.empty()) {
-            break;
-        }
-        cout << st << '\n';
-
+    char ch1, ch2;
+    float val;
+    while (cin >> ch1 >> ch2 >> val) {
+        //cout << val << '\n';
+        graph[static_cast<int>(ch1) - 97][static_cast<int>(ch2) - 97] = val;
+        //cout << graph[static_cast<int>(ch1) - 97][static_cast<int>(ch2) - 97] << '\n';
     }
-    cout << 0 << '\n';
 }
 
 class Node {
@@ -35,9 +32,9 @@ public:
 public:
     Node(char val = 0) :m_val(val) {
         m_came_from = 0;
-        m_path_val = 0;
-        m_everistic_val = 0;
-        m_total_val = 0;
+        m_path_val = 10000;
+        m_everistic_val = 10000;
+        m_total_val = 10000;
     }
 
     void setPathVal(float value) {
@@ -105,7 +102,7 @@ public:
         }
         
         for (int i = 0; i < table_size; i++) {
-            for (int j = 0; j < table_size; i++) {
+            for (int j = 0; j < table_size; j++) {
                 if (Atable[i][j] >= 0) {
                     m_data[i].appendNeighbour(m_data[j]);
                 }
@@ -141,29 +138,78 @@ bool cmp(Node node1, Node node2) {
         while (!open_set.empty()) {
             std::stable_sort(open_set.begin(), open_set.end(), cmp);
             current_node = open_set.back();
+            //cout << current_node.getValue() << '\n';
 
             if (current_node.getValue() == graph.m_data[finish].getValue()) {
                 path.push_back(current_node.getValue());
                 return path;
             }
+            
+            cout << current_node.getValue() << ": ";
+            for (int i = 0; i < open_set.size(); i++) {
+                cout << open_set[i].getValue() << ' ';
+            }
+            cout << '\n';
 
             open_set.pop_back();
             closed_set.push_back(current_node);
             for (int i = 0; i < current_node.m_neighbours.size(); i++) {
                 Node cur_neighbour = current_node.m_neighbours[i];
+                //cout << cur_neighbour.getValue() << '\n';
                 if (std::find(closed_set.begin(), closed_set.end(), cur_neighbour) != closed_set.end()) {
                     continue;
                 }
 
-                float temporal_path_val = cur_neighbour.getPathValue() + Atable[static_cast<int>(current_node.getValue() - 97)][static_cast<int>(cur_neighbour.getValue() - 97)];
-            }
+                float temporal_path_val = current_node.getPathValue() + Atable[static_cast<int>(current_node.getValue() - 97)][static_cast<int>(cur_neighbour.getValue() - 97)];
 
+                if (temporal_path_val < cur_neighbour.getPathValue()) {
+                    cur_neighbour.setPathVal(temporal_path_val);
+                    cur_neighbour.calcEveristic(graph.m_data[finish]);
+                    cur_neighbour.calcTotalValue();
+                }
+
+                if (std::find(open_set.begin(), open_set.end(), cur_neighbour) == open_set.end()) {
+                    open_set.push_back(cur_neighbour);
+                }
+                else {
+                    open_set.erase(std::find(open_set.begin(), open_set.end(), cur_neighbour));
+                    open_set.push_back(cur_neighbour);
+                }
+            }
         }
+
     }
 
     int main()
     {
-        int** graph = nullptr;
-        cout << "Input data:\n";
+        int size = 5;
+        int** Atable = new int* [size];
+        for (int i = 0; i < size; i++) {
+            Atable[i] = new int[size];
+        }
+        
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                Atable[i][j] = -1;
+            }
+        }
+
+        //cout << "Input data:\n";
+        getGraph(Atable);
+        
+        /*for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                cout << Atable[i][j] << ' ';
+            }
+            cout << "\n";
+        }*/
+
+        Graph graph(Atable, size);
+        std::vector<char> path = Astar(graph, 0, 4, Atable);
+        for (int i = 0; i < path.size(); i++) {
+            cout << path[i] << ' ';
+        }
+        cout << '\n';
+
         return 0;
     }
