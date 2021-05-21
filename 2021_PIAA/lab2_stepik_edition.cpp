773991d907ee2d607cd8b5ef9e395470e69d2aa3
LyubimovVladimir <55154058+VladimirLyubimov@users.noreply.github.com>
2021-05-21 11:54:07 +0300

    Update lab2_stepik_edition.cpp

--- 2021_PIAA/lab2_stepik_edition.cpp
+++ 2021_PIAA/lab2_stepik_edition.cpp
@@ -37,24 +37,10 @@ void increaseSize(int**& Atable, int new_length, int old_length) {
     Atable = new_table;
 }
 
-int getAtable(int**& Atable) {
-    int size = 0;
-    char ch1, ch2;
-    float val;
-    while (cin >> ch1 >> ch2 >> val) {
-        int max = std::max(static_cast<int>(ch1) - 96, static_cast<int>(ch2) - 96);
-        if (max > size) {
-            increaseSize(Atable, max, size);
-            size = max;
-        }
-        Atable[static_cast<int>(ch1) - 97][static_cast<int>(ch2) - 97] = val;
-    }
-    return size;
-}
-
 class Node {
 private:
     char m_val;
+    int m_index;
     int m_came_from;
     float m_path_val;
     float m_everistic_val;
@@ -66,6 +52,7 @@ public:
 public:
     Node(char val = 0) :m_val(val) {
         m_came_from = -1;
+        m_index = -1;
         m_path_val = 10000;
         m_everistic_val = 10000;
         m_total_val = 10000;
@@ -87,6 +74,14 @@ public:
         return m_came_from;
     }
 
+    void setIndex(int value) {
+        m_index = value;
+    }
+
+    int getIndex() {
+        return m_index;
+    }
+
     char getValue() {
         return m_val;
     }
@@ -106,42 +101,76 @@ public:
     void appendNeighbour(int node) {
         m_neighbours.push_back(node);
     }
-
-    friend bool operator== (const Node& node1, const Node& node2);
 };
-
-bool operator== (const Node& node1, const Node& node2) {
-    return (node1.m_val == node2.m_val);
-}
 
 class Graph {
 public:
     std::vector<Node> m_data;
 
 public:
-    Graph(int**& Atable, int table_size) {
-        for (int i = 0; i < table_size; i++) {
-            char node_val = static_cast<char>(i + 97);
-            m_data.push_back(Node(node_val));
-        }
+    Graph(int**& Atable) {
+        int size = 0;
+        int max = size;
+        char node1, node2;
+        float val;
+        std::vector<char> nodes;
+        while (cin >> node1 >> node2 >> val) {
+            if (std::find(nodes.begin(), nodes.end(), node1) == nodes.end() && std::find(nodes.begin(), nodes.end(), node2) == nodes.end()) {
+                max += 2;
+                nodes.push_back(node1);
+                nodes.push_back(node2);
+                m_data.push_back(Node(node1));
+                m_data[size].setIndex(size);
+                m_data.push_back(Node(node2));
+                m_data[size+1].setIndex(size+1);
+                m_data[size].appendNeighbour(size+1);
 
-        for (int i = 0; i < table_size; i++) {
-            for (int j = 0; j < table_size; j++) {
-                if (Atable[i][j] >= 0) {
-                    m_data[i].appendNeighbour(j);
-                }
+                increaseSize(Atable, max, size);
+                Atable[size][size + 1] = val;
+                size = max;
+                continue;
+            }
+
+            if (std::find(nodes.begin(), nodes.end(), node1) != nodes.end() && std::find(nodes.begin(), nodes.end(), node2) == nodes.end()) {
+                max += 1;
+                nodes.push_back(node2);
+                m_data.push_back(Node(node2));
+                m_data[size].setIndex(size);
+                m_data[findNode(node1)].appendNeighbour(size);
+
+                increaseSize(Atable, max, size);
+                Atable[findNode(node1)][size] = val;
+                size = max;
+                continue;
+            }
+
+            if (std::find(nodes.begin(), nodes.end(), node1) == nodes.end() && std::find(nodes.begin(), nodes.end(), node2) != nodes.end()) {
+                max += 1;
+                nodes.push_back(node1);
+                m_data.push_back(Node(node1));
+                m_data[size].setIndex(size);
+                m_data[size].appendNeighbour(findNode(node2));
+
+                increaseSize(Atable, max, size);
+                Atable[size][findNode(node2)] = val;
+                size = max;
+                continue;
+            }
+
+            if (std::find(nodes.begin(), nodes.end(), node1) != nodes.end() && std::find(nodes.begin(), nodes.end(), node2) != nodes.end()) {
+                m_data[findNode(node1)].appendNeighbour(findNode(node2));
+                Atable[findNode(node1)][findNode(node2)] = val;
+                continue;
             }
         }
     }
 
-    bool isNodeExist(char node_val) {
+    int findNode(char node_val) {
         for (int i = 0; i < m_data.size(); i++) {
             if (m_data[i].getValue() == node_val) {
-                return true;
+                return i;
             }
         }
-
-        return false;
     }
 };
 
@@ -178,7 +207,6 @@ std::vector<char> Astar(Graph graph, int start, int finish, int**& Atable) {
     std::vector<int> closed_set;
     std::vector<int> open_set;
     std::vector<char> path;
-    Node current_node;
 
     graph.m_data[start].setPathVal(0);
     graph.m_data[start].calcEveristic(graph.m_data[finish].getValue());
@@ -195,7 +223,6 @@ std::vector<char> Astar(Graph graph, int start, int finish, int**& Atable) {
         }
 
         for (int i = 0; i < graph.m_data[cur_node_index].m_neighbours.size(); i++) {
-            //if (std::find(closed_set.begin(), closed_set.end(), graph.m_data[cur_node_index].m_neighbours[i]) == closed_set.end()) {
             int cur_neighbour = graph.m_data[cur_node_index].m_neighbours[i];
 
             float temporal_path_val = graph.m_data[cur_node_index].getPathValue() + Atable[cur_node_index][cur_neighbour];
@@ -203,16 +230,14 @@ std::vector<char> Astar(Graph graph, int start, int finish, int**& Atable) {
 
             if (std::find(open_set.begin(), open_set.end(), cur_neighbour) == open_set.end()) {
                 open_set.push_back(cur_neighbour);
-                //need_update = true;
             }
-            //else {
+
             if (temporal_path_val < graph.m_data[cur_neighbour].getPathValue()) {
                 need_update = true;
             }
             else {
                 need_update = false;
             }
-            //}
 
             if (need_update) {
                 graph.m_data[cur_neighbour].setCameFrom(cur_node_index);
@@ -220,11 +245,8 @@ std::vector<char> Astar(Graph graph, int start, int finish, int**& Atable) {
                 graph.m_data[cur_neighbour].calcEveristic(graph.m_data[finish].getValue());
                 graph.m_data[cur_neighbour].calcTotalValue();
             }
-            //}
-            //graph.m_data[cur_node_index].m_neighbours.erase(std::find(graph.m_data[cur_node_index].m_neighbours.begin(), graph.m_data[cur_node_index].m_neighbours.end(), cur_neighbour));
         }
         graph.m_data[cur_node_index].m_neighbours.clear();
-
 
         int cur_node_index_copy = cur_node_index;
         closed_set.push_back(cur_node_index_copy);
@@ -235,16 +257,19 @@ std::vector<char> Astar(Graph graph, int start, int finish, int**& Atable) {
 int main() {
     int start, finish;
     int** Atable = nullptr;
-    int size;
     char ch1, ch2;
 
     cin >> ch1 >> ch2;
-    start = static_cast<int>(ch1) - 97;
-    finish = static_cast<int>(ch2) - 97;
 
-    size = getAtable(Atable);
-
-    Graph graph(Atable, size);
+    Graph graph(Atable);
+    start = graph.findNode(ch1);
+    finish = graph.findNode(ch2);
+    for (int i = 0; i < graph.m_data.size(); i++) {
+        for (int j = 0; j < graph.m_data.size(); j++) {
+            cout << Atable[i][j] << ' ';
+        }
+        cout << '\n';
+    }
     std::vector<char> path = Astar(graph, start, finish, Atable);
     for (int i = 0; i < path.size(); i++) {
         cout << path[i];
