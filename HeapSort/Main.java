5eaea76338be6119da037e47d4e52980853cc457
LyubimovVladimir <55154058+VladimirLyubimov@users.noreply.github.com>
2021-06-29 19:02:51 +0300

    Add files via upload

--- HeapSort/Main.java
+++ HeapSort/Main.java
@@ -4,9 +4,13 @@ import heap.*;
 public class Main{
 	public static void main(String[] args){
 		int[] arr = new int[] {18, 145, 54, -985, 554, 32, 8, 1, 2, 3, 6, 5, 4, 9, 8, 7, 1, 23, 6448, 35, 185, 188, 61, -8, -9, 3, -5, 0, 1452};
-		MaxHeap tree = new MaxHeap(arr, 8);
+		MaxHeap tree = new MaxHeap(arr, 2);
 		tree.printAll();
-		tree.makeHeap();
+		//tree.makeHeap();
 		tree.printAll();
+		tree.printAsHeap(null, 0);
+		//tree.heapSort();
+		//tree.printAll();
+		tree.maxSonPath();
 	}
 }
--- HeapSort/heap/MaxHeap.java
+++ HeapSort/heap/MaxHeap.java
@@ -15,9 +15,10 @@ public class MaxHeap{
 		size = this.data.getLength();
 		arr_size = size;
 		this.n = n;
+		makeHeap();
 	}
 	
-	public void makeHeap(){
+	private void makeHeap(){
 		for(int i = size/n; i >= 0; i--){
 			siftDown(i);
 		}
@@ -40,7 +41,7 @@ public class MaxHeap{
 		return max;
 	}
 	
-	public void siftDown(int index){
+	private void siftDown(int index){
 		while(n*index + 1 < size){
 			int max_son = getMaxSonIndex(index);
 			if(max_son == -1){
@@ -58,7 +59,137 @@ public class MaxHeap{
 		}
 	}
 	
-//------------------------------	
+	private int getHeight(){
+		int height = 0;
+		int node = 0;
+		while(node < size){
+			node = node*n + 1;
+			height += 1;
+		}
+		
+		return height;
+	}
+	
+	public void heapSort(){
+		while (size > 1){
+			int temp_val = data.getElem(size - 1);
+			data.replaceElem(data.getElem(0), size-1);
+			data.replaceElem(temp_val, 0);
+			size -= 1;
+			siftDown(0);
+		}
+	}
+	
+	public int[] maxSonPath(){
+		int root = 0;
+		int[] way = new int[getHeight()];
+		int i = 0;
+		
+		while(root < size && root != -1){
+			way[i] = root;
+			i += 1;
+			root = getMaxSonIndex(root);
+			printAsHeap(way, way.length);
+		}
+		
+		return way;
+	}
+	
+//------------------------------
+
+	public void printNode(int node_value, int step, boolean is_col, char side){//выводит узел в консоль
+		if(side == 'l' || side == 't'){
+			for(int i = 0; i < step; i++){
+				System.out.print(' ');
+			}
+		}
+		else{
+			for(int i = 0; i < step; i++){
+				System.out.print("\033[1;32m");
+				System.out.print('-');
+			}
+		}
+
+		if(is_col){
+			System.out.print("\033[1;30;41m");
+		}
+		else{
+			System.out.print("\033[1;30;47m");
+		}
+		
+		System.out.format("%-4d", node_value);
+		System.out.print("\033[0m");
+
+		if(side == 'r' || side == 't'){
+			for(int i = 0; i < step; i++){
+				System.out.print(' ');
+			}
+		}
+		else{
+			for(int i = 0; i < step; i++){
+				System.out.print("\033[1;32m");
+				System.out.print('-');
+			}
+		}
+		System.out.print("\033[0m");
+	}
+
+	public void printAsHeap(int[] color_nodes, int col_size){
+		if(size == 0){
+			System.out.println("Empty heap!");
+			return;
+		}
+		
+		int lev = 0;
+		int height = getHeight();
+		boolean is_col = false;
+		int j = 0;
+		char side = 0;
+			
+		int step = 0;
+		for(int i = 0; i < size; i++){
+			step = (int)(3*2*(int)(Math.pow((double)n, (double)(height-1)))/(2*(int)(Math.pow((double)n, (double)lev)))-2);
+				
+			is_col = false;
+			if(j < col_size){
+				if(i == color_nodes[j]){
+					is_col = true;
+					j += 1;
+				}
+			}
+				
+			if(lev == 0){
+				printNode(data.getElem(i), step, is_col, 't');
+				lev += 1;
+				System.out.print("|||||\n");
+				continue;
+			}
+
+			if(i%n == 1){
+				side = 'l';
+			}
+			
+			if(i%n == 0 || i == size-1){
+				side = 'r';
+			}
+
+			if(n == 1 || (i == size-1 && i%n == 1)){
+				side = 't';
+			}
+			printNode(data.getElem(i), step, is_col, side);
+			side = 0;
+
+			if(i%((int)((double)(1.0-Math.pow((double)n, (double)(lev+1)))/(double)(1-n))-1) == 0 || n == 1){
+				lev += 1;
+				System.out.print("|||||\n");
+			}
+		}
+		System.out.println();
+		System.out.println();
+		System.out.println();
+	}
+	
+	
 	public void printAll(){
 		data.print(0, arr_size);
 	}
