9a8b0b73654444c0eae57dc84d787b405479e96a
LyubimovVladimir <55154058+VladimirLyubimov@users.noreply.github.com>
2021-06-30 16:12:47 +0300

    Add files via upload

--- HeapSort/Main.java
+++ HeapSort/Main.java
@@ -1,16 +1,19 @@
 import myarr.*;
 import heap.*;
+import fileio.*;
 
 public class Main{
 	public static void main(String[] args){
-		int[] arr = new int[] {18, 145, 54, -985, 554, 32, 8, 1, 2, 3, 6, 5, 4, 9, 8, 7, 1, 23, 6448, 35, 185, 188, 61, -8, -9, 3, -5, 0, 1452};
+		//int[] arr = new int[] {18, 145, 54, -985, 554, 32, 8, 1, 2, 3, 6, 5, 4, 9, 8, 7, 1, 23, 6448, 35, 185, 188, 61, -8, -9, 3, -5, 0, 1452};
+		int [] arr = FileIO.readArrayFromFile("./test.txt");
 		MaxHeap tree = new MaxHeap(arr, 2);
-		tree.printAll();
+		//tree.printAll();
 		//tree.makeHeap();
-		tree.printAll();
+		//tree.printAll();
 		tree.printAsHeap(null, 0);
 		//tree.heapSort();
 		//tree.printAll();
 		tree.maxSonPath();
+		//System.out.println(arr);
 	}
 }
---
+++ HeapSort/fileio/FileIO.java
@@ -0,0 +1,46 @@
+package fileio;
+
+import java.util.Scanner;
+import java.io.*;
+import myarr.MyDynamicIntArray;
+
+public class FileIO{
+	public static int[] readArrayFromFile(String filename){
+		MyDynamicIntArray arr = new MyDynamicIntArray();
+		int[] res = null;
+		try{
+			FileReader fin = new FileReader(filename);
+			int c;
+			while((c = fin.read()) != -1){
+				arr.add(c);
+				//System.out.print(c + " ");
+			}			
+			//System.out.println();
+			fin.close();
+		}
+		catch(IOException err){
+			System.out.println(err.getMessage());
+			return res;
+		}
+		
+		if(arr.getLength() > 0){
+			char[] ch_st = new char[arr.getLength()];
+			for(int i = 0; i < arr.getLength(); i++){
+				ch_st[i] = (char) arr.getElem(i);
+			}
+			String st = new String(ch_st);
+			String[] values = st.split("\\s");
+			res = new int[values.length];
+			int i = 0;
+			while(i < res.length){
+				res[i] = Integer.parseInt(values[i]);
+				i++;
+			}
+		}
+		else{
+			return res;
+		}
+		
+		return res;
+	}
+}
